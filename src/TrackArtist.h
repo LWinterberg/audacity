bc776accada787d95d14ae1ed16243751eac878e
LWinterberg <87814144+LWinterberg@users.noreply.github.com>
2024-03-20 11:20:36 +0100

    remove unused Shadow

--- src/TrackArtist.h
+++ src/TrackArtist.h
@@ -97,7 +97,6 @@ public:
    wxPen muteSamplePen;
    wxPen odProgressNotYetPen;
    wxPen odProgressDonePen;
-   wxPen shadowPen;
    wxPen clippedPen;
    wxPen muteClippedPen;
    wxPen blankSelectedPen;
